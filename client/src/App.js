e1e6448c517ae077975526acd81e7828ff6822d2
Vidhunkishor <168447729+Vidhunkishor@users.noreply.github.com>
2025-03-29 18:24:32 +0530

    Add client/src/App.js 1743252873356

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute-v6';
 import StudentList from './components/StudentList';
 import Attendance from './components/Attendance';
 import AdminLogin from './components/AdminLogin';
 import Navbar from './components/Navbar';
 
 function App() {
-  const [isAdmin, setIsAdmin] = useState(false);
-
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
-        <Navbar isAdmin={isAdmin} setIsAdmin={setIsAdmin} />
-        <div className="container mx-auto p-4">
-          <Routes>
-            <Route path="/" element={<Attendance />} />
-            <Route 
-              path="/students" 
-              element={isAdmin ? <StudentList /> : <AdminLogin setIsAdmin={setIsAdmin} />} 
-            />
-          </Routes>
+      <AuthProvider>
+        <div className="min-h-screen bg-gray-100">
+          <Navbar />
+          <div className="container mx-auto p-4">
+            <Routes>
+              <Route path="/" element={<Attendance />} />
+              <Route path="/login" element={<AdminLogin />} />
+              <Route 
+                path="/students" 
+                element={
+                  <ProtectedRoute>
+                    <StudentList />
+                  </ProtectedRoute>
+                } 
+              />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </AuthProvider>
     </Router>
   );
 }
